0cc2529be77d22501e4a6a9fcc4fa0ae977373fd
ilia-wolke7 <ilia@wolke7.net>
2023-03-15 02:22:14 +0100

    Added simple BadgerNFT

--- contracts/BadgerNFT.sol
+++ contracts/BadgerNFT.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BadgerCoin is ERC721 {
-    constructor() ERC721("BadgerNFT", "BC") {
+contract BadgerNFT is ERC721 {
+    constructor() ERC721("BadgerNFT", "BNFT") {
       
     }
 }
